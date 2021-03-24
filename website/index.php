7558ae26af5bf944455373cdc4c38f061d182e65
caprigit <amrish1982@gmail.com>
2021-03-24 15:18:44 +0000

    my commit

--- website/index.php
+++ website/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "EMERSON DEMO by Amrish today Varun Kaushal Bhai"; 
+echo "EMERSON DEMO by Amrish today Varun Kaushal and Amit Dutt"; 
 
 require 'config.php';
 require 'functions.php';
